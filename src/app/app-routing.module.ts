91015f88fe35eba7a6d24d1147bae841f2b899e5
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-16 22:00:28 +0200

    ADD, routes to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard-component/dashboard.component';
+import { LoginComponent } from './components/login-component/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // redirect to `first-component`
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
