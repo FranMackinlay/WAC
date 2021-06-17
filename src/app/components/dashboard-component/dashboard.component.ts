abf540d0f9bbdcc1354ca603b3d6de6e9f981305
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-17 12:52:44 +0200

    IMPROVE, dashboard component to consume services and display

--- src/app/components/dashboard-component/dashboard.component.ts
+++ src/app/components/dashboard-component/dashboard.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Product, Products } from '../../core.contstant';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { trigger, transition, style, animate, state } from '@angular/animations';
+import { ProductsSrv, Product } from 'src/app/services/product.service';
 
 
-const TABLE_DATA: Product[] = Products;
+
 
 @Component({
   selector: 'app-dashboard',
@@ -24,23 +25,56 @@ const TABLE_DATA: Product[] = Products;
 })
 export class DashboardComponent implements OnInit {
 
-  state: string = 'closed';
-  displayedColumns: string[] = ['name', 'status', 'edit'];
-  dataSource = TABLE_DATA;
+  state!: string;
+  displayedColumns: string[] = [];
+  dataSource!: Product[];
+  selectedProduct!: Product;
+  productForm!: FormGroup;
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  constructor(private fb: FormBuilder, private productService: ProductsSrv, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.state = 'closed';
+    this.displayedColumns = ['name', 'status', 'edit'];
+    this.productService.getProducts().subscribe((response: any) => {
+      this.dataSource = response.products;
+    });
   }
 
 
   onClickEdit(element: any) {
-    console.log(`element`, element);
+    if (this.selectedProduct && element.id !== this.selectedProduct.id) this.changeState();
+    this.selectedProduct = element;
+    this.hydrateForm(this.selectedProduct)
     this.changeState();
+  }
+
+  hydrateForm(product: Product) {
+    this.productForm = this.fb.group({
+      id: [product.id, [Validators.required]],
+      name: [product.name, [Validators.required, Validators.minLength(3)]],
+      status: [product.status, [Validators.required, Validators.minLength(6)]],
+      color: [product.color, [Validators.required, Validators.minLength(3)]],
+      description: [product.description, [Validators.required, Validators.minLength(5)]],
+      created_at: [product.created_at, [Validators.required, Validators.minLength(3)]],
+      updated_at: [product.updated_at, [Validators.required, Validators.minLength(3)]],
+    })
   }
 
   changeState(): void {
     (this.state == "closed") ? this.state = "open" : this.state = "closed";
+  }
+
+  onSubmit(): void {
+    if (!this.productForm.valid) {
+      return alert('Please check the form again.');
+    }
+    this.productService.updateProduct(this.productForm.value).subscribe(data => {
+      this.dataSource = data.products;
+      this.changeState();
+    });
   }
 
 }
