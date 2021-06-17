5aba78497285694359f2ebb02a5792b507904340
Francisco Mackinlay <franmackinlay@gmail.com>
2021-06-17 16:51:03 +0200

    IMPROVE, dashboard compnents

--- src/app/components/dashboard-component/dashboard.component.ts
+++ src/app/components/dashboard-component/dashboard.component.ts
@@ -3,8 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { trigger, transition, style, animate, state } from '@angular/animations';
 import { ProductsSrv } from 'src/app/services/product.service';
 import { Product } from 'src/app/interfaces/product.interface';
+import { Router } from '@angular/router';
 import { HttpProductResponse } from 'src/app/interfaces/http-product-response.interface';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,9 +15,12 @@ import { ActivatedRoute, Router } from '@angular/router';
       state('closed', style({
         width: 0,
         opacity: 0,
+        position: 'absolute',
+        right: '-20em'
       })),
       state('open', style({
         opacity: 1,
+        position: 'unset',
       })),
       transition('* => *', animate(150))
     ]),
@@ -25,56 +28,34 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
+
   state!: string;
-  displayedColumns: string[] = [];
-  dataSource!: Product[];
   selectedProduct!: Product;
-  productForm!: FormGroup;
+  products!: Product[];
 
-
-
-  constructor(private fb: FormBuilder, private productService: ProductsSrv, private cdr: ChangeDetectorRef, private route: Router, private activatedRoute: ActivatedRoute) { }
+  constructor(private productService: ProductsSrv, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
-    if (!window.history.state.data?.token) this.route.navigate(['/login']);
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    // if (!window.history.state.data?.token || !user.token) this.route.navigate(['/login']);
     this.state = 'closed';
-    this.displayedColumns = ['name', 'status', 'edit'];
-    this.productService.getProducts().subscribe((response: any) => {
-      this.dataSource = response.products;
-    });
   }
 
-
-  onClickEdit(element: any) {
-    if (this.selectedProduct && element.id !== this.selectedProduct.id) this.changeState();
-    this.selectedProduct = element;
-    this.hydrateForm(this.selectedProduct)
+  onChangeSelectedProduct($event: Product) {
+    if (this.selectedProduct && this.selectedProduct.id !== $event.id) this.changeState();
+    this.selectedProduct = $event;
     this.changeState();
-  }
 
-  hydrateForm(product: Product) {
-    this.productForm = this.fb.group({
-      id: [product.id, [Validators.required]],
-      name: [product.name, [Validators.required, Validators.minLength(3)]],
-      status: [product.status, [Validators.required, Validators.minLength(6)]],
-      color: [product.color, [Validators.required, Validators.minLength(3)]],
-      description: [product.description, [Validators.required, Validators.minLength(5)]],
-      created_at: [product.created_at, [Validators.required, Validators.minLength(3)]],
-      updated_at: [product.updated_at, [Validators.required, Validators.minLength(3)]],
-    })
   }
 
   changeState(): void {
     (this.state == "closed") ? this.state = "open" : this.state = "closed";
   }
 
-  onSubmit(): void {
-    if (!this.productForm.valid) {
-      return alert('Please check the form again.');
-    }
 
-    this.productService.updateProduct(this.productForm.value).subscribe((data: HttpProductResponse) => {
-      this.dataSource = data.products;
+  onSaveChanges($event: any) {
+    this.productService.updateProduct($event).subscribe((data: HttpProductResponse) => {
+      this.products = data.products;
       this.changeState();
     });
   }
